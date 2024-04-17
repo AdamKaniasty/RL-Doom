cc10efb576026f331bb1a7cd98298dbcda3d9688
Igor Kołodziej <igor.kolodziej@proton.me>
2024-04-17 21:49:12 +0200

    fix modules

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import vizdoom as vzd
 import os
 from argparse import ArgumentParser
 from multiprocessing import Process
-from game.play import play
-from game.train import train
+from src.game.play import play
+from src.game.train import train
 
 CONFIG_PATH = os.path.join(vzd.scenarios_path, "deadly_corridor.cfg")
 
