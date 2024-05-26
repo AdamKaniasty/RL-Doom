469c3c829692d63f5946c62f018bf80bc76744aa
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2024-05-26 19:40:33 +0200

    moving gives reward

--- src/rewards/reward_abstract.py
+++ src/rewards/reward_abstract.py
@@ -1,6 +1,6 @@
 class AbstractReward:
     def __init__(self, health_reward=0.1, damage_penalty=-0.1, enemy_reward=1.0, goal_reward=10.0, ammo_penalty=-0.01,
-                 death_penalty=0):
+                 death_penalty=0, moving_x_reward=0.001, moving_y_reward=0.001, moving_z_reward=0.001):
         # Define reward values
         self.health_reward = health_reward
         self.damage_penalty = damage_penalty
@@ -8,11 +8,17 @@ class AbstractReward:
         self.goal_reward = goal_reward
         self.ammo_penalty = ammo_penalty
         self.death_penalty = death_penalty
+        self.moving_x_reward = moving_x_reward
+        self.moving_y_reward = moving_y_reward
+        self.moving_z_reward = moving_z_reward
         self.first_run = True
         self.prev_health = 0
         self.prev_ammo = 0
         self.prev_kills = 0
-        self.cumulated_reward = 0.0
+        self.prev_x = 0
+        self.prev_y = 0
+        self.prev_z = 0
+        self.cumulated_reward = 0.0  # it is cumulated reward with discount factor = 1
 
     def evaluate(self, game_state):
         if game_state is None:
@@ -22,6 +28,9 @@ class AbstractReward:
             self.prev_health = game_state[1]
             self.prev_ammo = game_state[0]
             self.prev_kills = game_state[2]
+            self.prev_x = game_state[3]
+            self.prev_y = game_state[4]
+            self.prev_z = game_state[5]
             self.first_run = False
             return 0.0
         reward = 0.0
@@ -29,9 +38,16 @@ class AbstractReward:
         health_change = game_state[1] - self.prev_health
         ammo_change = game_state[0] - self.prev_ammo
         kills_change = game_state[2] - self.prev_kills
+        #abs change in x, y, z
+        x_change = abs(game_state[3] - self.prev_x)
+        y_change = abs(game_state[4] - self.prev_y)
+        z_change = abs(game_state[5] - self.prev_z)
         reward += health_change * self.health_reward
         reward += ammo_change * self.ammo_penalty
         reward += kills_change * self.enemy_reward
+        reward += x_change * self.moving_x_reward
+        reward += y_change * self.moving_y_reward
+        reward += z_change * self.moving_z_reward
         if game_state[-1] != 0:
             # Player died
             reward += self.death_penalty
--- src/rewards/reward_survivaility.py
+++ src/rewards/reward_survivaility.py
@@ -2,6 +2,8 @@ from src.rewards.reward_abstract import AbstractReward
 
 
 class SurvivalReward(AbstractReward):
-    def __init__(self, health_reward=0.1, damage_penalty=-0.5, enemy_reward=1.0, goal_reward=10.0, ammo_penalty=-0.01,
-                 death_penalty=-10.0):
-        super().__init__(health_reward, damage_penalty, enemy_reward, goal_reward, ammo_penalty, death_penalty)
+
+    def __init__(self, health_reward=0.1, damage_penalty=-0.1, enemy_reward=1.0, goal_reward=10.0, ammo_penalty=-0.01,
+                 death_penalty=-10.0, moving_x_reward=0.001, moving_y_reward=0.001, moving_z_reward=0.001):
+        super().__init__(health_reward, damage_penalty, enemy_reward, goal_reward, ammo_penalty, death_penalty,
+                         moving_x_reward, moving_y_reward, moving_z_reward)
