1034f8ddb23048724a5f2847a69eefbff1e44e13
Igor Kołodziej <igor.kolodziej@proton.me>
2024-06-01 12:07:19 +0200

    Kiroshi Optics Mk.1

--- src/utils/screen_preprocess.py
+++ src/utils/screen_preprocess.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 import gymnasium as gym
 from gymnasium.spaces import Box, Dict
+from src.game.gymnasium_wrapper import VizDOOM
+import matplotlib.pyplot as plt
 
 
 class PreprocessFrameAndGameVariables(gym.ObservationWrapper):
-    def __init__(self, env, screen_shape=(150, 300)):
+    def __init__(self, env, screen_shape=(160, 90)):
         super(PreprocessFrameAndGameVariables, self).__init__(env)
         self.screen_shape = screen_shape
         self.observation_space = Dict({
@@ -20,9 +22,50 @@ class PreprocessFrameAndGameVariables(gym.ObservationWrapper):
         }
 
     def _process_screen(self, screen):
-        if len(screen.shape) == 3 and screen.shape[2] == 3:
-            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        if len(screen.shape) == 4:
+            screen = screen.squeeze()
+        screen = np.transpose(screen, (1, 2, 0))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        screen = self._crop(screen, 30, 30)
         screen = cv2.resize(screen, self.screen_shape, interpolation=cv2.INTER_AREA)
-
+        screen = cv2.GaussianBlur(screen, (5, 5), 0)
+        screen = cv2.equalizeHist(screen)
+        screen = cv2.resize(screen, self.screen_shape, interpolation=cv2.INTER_AREA)
+        screen = self._add_crosshair(screen, 8)
         screen = np.expand_dims(screen, axis=-1)
+
         return screen
+
+    def _add_crosshair(self, image, size=10):
+        center_x, center_y = image.shape[1] // 2, image.shape[0] // 2
+        cv2.line(image, (center_x - size, center_y), (center_x + size, center_y), (255, 255, 255), 1)
+        cv2.line(image, (center_x, center_y - size), (center_x, center_y + size), (255, 255, 255), 1)
+
+        return image
+
+    def _crop(self, image, top, down):
+        return image[top:-down, :]
+
+
+if __name__ == '__main__':
+    env = VizDOOM(level='deadly_corridor')
+    env = PreprocessFrameAndGameVariables(env)
+    env.reset()
+
+    frames = []
+    for _ in range(3):
+        state, reward, done, info, _ = env.step(env.action_space.sample())
+        print(f'Screen shape: {state["screen"].shape}')
+        print(f'Game variables: {state["gamevariables"]}')
+        print(f'Reward: {reward}')
+        print(f'Done: {done}')
+        print(f'Info: {info}')
+        print()
+        frames.append(state['screen'])
+
+    plt.figure(figsize=(15, 10))
+    for i, frame in enumerate(frames):
+        plt.subplot(1, len(frames), i + 1)
+        plt.imshow(frame, cmap='gray')
+        plt.axis('off')
+    plt.show()
