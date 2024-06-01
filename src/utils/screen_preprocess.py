520179e3ac5ff739b6c1c9712813eae46dddca2e
Igor Kołodziej <igor.kolodziej@proton.me>
2024-06-01 13:55:44 +0200

    Kiroshi Optics Mk.2

--- src/utils/screen_preprocess.py
+++ src/utils/screen_preprocess.py
@@ -7,17 +7,21 @@ import matplotlib.pyplot as plt
 
 
 class PreprocessFrameAndGameVariables(gym.ObservationWrapper):
-    def __init__(self, env, screen_shape=(160, 90)):
+    def __init__(self, env, screen_shape=(256, 144)):
         super(PreprocessFrameAndGameVariables, self).__init__(env)
         self.screen_shape = screen_shape
         self.observation_space = Dict({
-            'screen': Box(low=0, high=255, shape=(self.screen_shape[1], self.screen_shape[0], 1), dtype=np.uint8),
+            'screen': Box(low=0, high=255, shape=(self.screen_shape[1], self.screen_shape[0], 2), dtype=np.uint8),
             'gamevariables': env.observation_space['gamevariables']
         })
 
     def observation(self, obs):
+        screen = obs['screen']
+        processed_screen = self._process_screen(screen)
+        edges = self._detect_edges(screen)
+        combined_screen = np.concatenate((processed_screen, edges), axis=-1)
         return {
-            'screen': self._process_screen(obs['screen']),
+            'screen': combined_screen,
             'gamevariables': obs['gamevariables']
         }
 
@@ -27,20 +31,28 @@ class PreprocessFrameAndGameVariables(gym.ObservationWrapper):
         screen = np.transpose(screen, (1, 2, 0))
         screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
         screen = self._crop(screen, 30, 30)
-        screen = cv2.resize(screen, self.screen_shape, interpolation=cv2.INTER_AREA)
-        screen = cv2.GaussianBlur(screen, (5, 5), 0)
+        screen = cv2.GaussianBlur(screen, (3, 3), 0)
         screen = cv2.equalizeHist(screen)
         screen = cv2.resize(screen, self.screen_shape, interpolation=cv2.INTER_AREA)
-        screen = self._add_crosshair(screen, 8)
+        screen = self._add_crosshair(screen, 6)
         screen = np.expand_dims(screen, axis=-1)
-
         return screen
+
+    def _detect_edges(self, screen):
+        if len(screen.shape) == 4:
+            screen = screen.squeeze()
+        screen = np.transpose(screen, (1, 2, 0))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(screen, 80, 180)
+        edges = self._crop(edges, 30, 30)
+        edges = cv2.resize(edges, self.screen_shape, interpolation=cv2.INTER_AREA)
+        edges = np.expand_dims(edges, axis=-1)
+        return edges
 
     def _add_crosshair(self, image, size=10):
         center_x, center_y = image.shape[1] // 2, image.shape[0] // 2
         cv2.line(image, (center_x - size, center_y), (center_x + size, center_y), (255, 255, 255), 1)
         cv2.line(image, (center_x, center_y - size), (center_x, center_y + size), (255, 255, 255), 1)
-
         return image
 
     def _crop(self, image, top, down):
@@ -48,7 +60,7 @@ class PreprocessFrameAndGameVariables(gym.ObservationWrapper):
 
 
 if __name__ == '__main__':
-    env = VizDOOM(level='deadly_corridor')
+    env = gym.make('Vizdoom-v0', level='deadly_corridor')
     env = PreprocessFrameAndGameVariables(env)
     env.reset()
 
@@ -65,7 +77,13 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(15, 10))
     for i, frame in enumerate(frames):
-        plt.subplot(1, len(frames), i + 1)
-        plt.imshow(frame, cmap='gray')
+        plt.subplot(2, len(frames), i + 1)
+        plt.imshow(frame[:, :, 0], cmap='gray')
+        plt.title('Processed Screen')
+        plt.axis('off')
+
+        plt.subplot(2, len(frames), i + len(frames) + 1)
+        plt.imshow(frame[:, :, 1], cmap='gray')
+        plt.title('Edges')
         plt.axis('off')
     plt.show()
