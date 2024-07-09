2bf2e0887d9af3b42e8c81cfbdd10c3921023b55
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2024-07-09 20:32:57 +0200

    not needed rewards remove

--- src/rewards/reward_killing.py
+++
@@ -1,9 +0,0 @@
-from src.rewards.reward_abstract import AbstractReward
-
-
-class KillingReward(AbstractReward):
-
-    def __init__(self, health_reward=0.2, damage_penalty=-0.2, enemy_reward=2.0, goal_reward=10.0, ammo_penalty=-0.01,
-                 death_penalty=0, moving_x_reward=0.001, moving_y_reward=0.001, moving_z_reward=0.001):
-        super().__init__(health_reward, damage_penalty, enemy_reward, goal_reward, ammo_penalty, death_penalty,
-                         moving_x_reward, moving_y_reward, moving_z_reward)
